6129986df7cc9191b35168fe507ee2cb161f5a36
Robério Almeida <dev.roberio@gmail.com>
2022-03-22 18:39:25 -0300

    feat: testa se retorna mensagem default caso não existam pokemons nos favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test favorites page', () => {
+  beforeEach(() => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+
+  it('Test is(No favorite pokemon found)', () => {
+    const noFav = screen.getByText(/No favorite pokemon found/i);
+    expect(noFav).toBeInTheDocument();
+  });
+});
