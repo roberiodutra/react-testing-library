5a9f2cdecb710643904cf164c24b972fe677aefc
Robério Almeida <dev.roberio@gmail.com>
2022-03-22 17:51:09 -0300

    feat: procura o heading da página About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Test about page', () => {
+  beforeEach(() => render(<About />));
+
+  it('Test about title', () => {
+    const heading = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+  });
+});
