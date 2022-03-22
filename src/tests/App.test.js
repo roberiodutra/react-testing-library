7c43f5fe55758566a0ce401365f11b34d79433dd
Robério Almeida <dev.roberio@gmail.com>
2022-03-22 15:42:40 -0300

    feat: testa se o link home direciona para /

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,26 @@
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../components/renderWithRouter';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-describe('Teste o componente <App.js />', () => {
-  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+describe('Test App component', () => {
+  beforeEach(() => {
     renderWithRouter(<App />);
+  });
+
+  it('Teste nav links', () => {
     const nav = screen.getByRole('navigation');
     const home = screen.getByText('Home');
     const about = screen.getByText('About');
     const favorite = screen.getByText('Favorite Pokémons');
 
     expect(nav && home && about && favorite).toBeInTheDocument();
+  });
+
+  it('Redirect to Home', () => {
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    userEvent.click(homeLink);
+    const heading = screen.getByText("Encountered pokémons");
+    expect(heading).toBeInTheDocument();
   });
 });
