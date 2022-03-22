f338bd5c36a9e52515ebad77576867482102396c
Robério Almeida <dev.roberio@gmail.com>
2022-03-22 18:11:11 -0300

    feat: confere se existem dois paragrafos na página About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,4 +9,9 @@ describe('Test about page', () => {
     const heading = screen.getByRole('heading', { name: 'About Pokédex' });
     expect(heading).toBeInTheDocument();
   });
+
+  it('Test if there are two paragraphs', () => {
+    const paragraph = screen.getAllByText(/Pokémons/i);
+    expect(paragraph[0] && paragraph[1]).toBeInTheDocument();
+  });
 });
