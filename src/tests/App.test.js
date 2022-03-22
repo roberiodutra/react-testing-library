5f50ed94a2b19f40477c3a1cb743a189a38db07a
Robério Almeida <dev.roberio@gmail.com>
2022-03-22 15:21:14 -0300

    feat: cria o teste para o menu de navegação no component App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+    renderWithRouter(<App />);
+    const navigation = screen.getByRole('navigation');
+    expect(navigation).toBeInTheDocument();
+
+    const Home = screen.getByText('Home');
+    const About = screen.getByText('About');
+    const Favorite = screen.getByText('Favorite Pokémons');
+
+    expect(Home && About && Favorite).toBeInTheDocument();
+  });
+});
