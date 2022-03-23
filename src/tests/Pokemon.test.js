3d289bb5ebdd92fbdfce00e5ef575bc58ea2b84d
Robério Almeida <dev.roberio@gmail.com>
2022-03-23 02:05:56 -0300

    feat: verifica se o card do charmander é renderizado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente Pokemon', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Test if a card with info is rendered', () => {
+    const photo = 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png';
+    const nextPokemon = screen.getByTestId('next-pokemon');
+    userEvent.click(nextPokemon);
+
+    const detail = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(detail);
+
+    const charm = screen.getByTestId('pokemon-name');
+    expect(charm).toHaveTextContent(/charmander/i);
+
+    const charmType = screen.getByTestId('pokemon-type');
+    expect(charmType).toHaveTextContent(/fire/i);
+
+    const charmWeight = screen.getByTestId('pokemon-weight');
+    expect(charmWeight).toHaveTextContent('Average weight: 8.5 kg');
+
+    const charmPic = screen.getByRole('img', { name: /Charmander sprite/i });
+    expect(charmPic).toHaveAttribute('src', photo);
+  });
+});
