14fdd7aaaafb97ba9bf0a80ef86172c686174c11
Robério Almeida <dev.roberio@gmail.com>
2022-03-23 00:17:33 -0300

    feat: testa o botão de proximo pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testa a pagina de Pokedex', () => {
   beforeEach(() => {
-    renderWithRouter(<App />)
+    renderWithRouter(<App />);
   });
 
   it('Test if h2 shows "Encountered pokémons" in the document', () => {
     const heading = screen.getByRole('heading', { name: /Encountered/i, level: 2 });
     expect(heading).toBeInTheDocument();
+  });
+
+  it('Test if "Próximo pokémon" is working', () => {
+    const nextPokemon = screen.getByTestId('next-pokemon');
+    userEvent.click(nextPokemon);
+    const next = screen.getByTestId('pokemon-name');
+    expect(next).toHaveTextContent(/Charmander/i);
+    expect(nextPokemon.innerHTML).toBe('Próximo pokémon');
+    expect(nextPokemon).toBeInTheDocument();
+    const seven = 7;
+
+    for (let i = 0; i < seven; i += 1) {
+      userEvent.click(nextPokemon);
+      if (i === seven) {
+        expect(next).toHaveTextContent(/Pikachu/i);
+      }
+    }
   });
 });
